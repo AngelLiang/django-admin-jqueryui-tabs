10c9318b471317c1dabfaa813b9dabc2045cccd5
AngelLiang <pl01665077@163.com>
2020-09-20 10:00:25 +0800

    demo project CustomUserAdmin class added fieldsets attribute

--- apps/customauth/admin.py
+++ apps/customauth/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext, gettext_lazy as _
 from django.contrib.auth import get_user_model
 
 from django_admin_jqueryui_tabs.mixins import ModelAdminTabsMixin
@@ -8,7 +9,20 @@ User = get_user_model()
 
 
 class CustomUserAdmin(ModelAdminTabsMixin, UserAdmin):
-    pass
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password'),
+        }),
+        (_('Personal info'), {
+            'fields': ('first_name', 'last_name', 'email'),
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {
+            'fields': ('last_login', 'date_joined'),
+        }),
+    )
 
 
 admin.site.unregister(User)
